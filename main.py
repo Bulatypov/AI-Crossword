16cafa3fd4ac855a74be76ec701670bf936e3d9d
BuLatypov <bulat2747@gmail.com>
2023-12-01 04:39:26 +0300

    Not okay, but partially done

--- main.py
+++ main.py
@@ -14,9 +14,139 @@ def find_possible_intersections(arr):
     return ans
 
 
-def check_crossword(intersections_par, real_intersections_par=None):
+def check_crossword_build(real_intersections_par):
+    map_crossword = [['.' for _ in range(100)] for _ in range(100)]
+    if not real_intersections_par:
+        return map_crossword
+    real_intersections = copy.deepcopy(real_intersections_par)
+    num_crossword = [[[] for _ in range(100)] for _ in range(100)]
+    placed_words = [[real_intersections[0][0], 50, 50, True]]
+    used_words = [real_intersections[0][0]]
+
+    def place_word(word, row, column, vertical):
+        current_word_num = len(used_words) - 1
+
+        def is_start(word_ind, x_coord, y_coord):
+            return placed_words[word_ind][1] == x_coord and placed_words[word_ind][2] == y_coord
+
+        def is_finish(word_ind, x_coord, y_coord):
+            return (placed_words[word_ind][1] + (len(inp[placed_words[word_ind][0]]) - 1) *
+                    placed_words[word_ind][3] == x_coord and
+                    placed_words[word_ind][2] + (len(inp[placed_words[word_ind][0]]) - 1) *
+                    (not placed_words[word_ind][3]) == y_coord)
+
+        letter_ind = 0
+        for x in range(row, row + (len(word) - 1) * vertical + 1):
+            for y in range(column, column + (len(word) - 1) * (not vertical) + 1):
+                if map_crossword[x][y] == '.' or map_crossword[x][y] == word[letter_ind]:
+                    map_crossword[x][y] = word[letter_ind]
+                    num_crossword[x][y].append(current_word_num)
+                    letter_ind += 1
+                else:
+                    return False
+
+        for x in range(row, row + (len(word) - 1) * vertical + 1):
+            for y in range(column, column + (len(word) - 1) * (not vertical) + 1):
+                neighbours = [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]]
+                for neighbour in neighbours:
+                    n_x = neighbour[0]
+                    n_y = neighbour[1]
+                    if num_crossword[n_x][n_y] == num_crossword[x][y] or not num_crossword[n_x][n_y]:
+                        continue
+                    elif len(num_crossword[n_x][n_y]) > 2 or len(num_crossword[x][y]) > 2:
+                        return False
+                    elif list(set(num_crossword[x][y]) & set(num_crossword[n_x][n_y])):
+                        all_words = copy.deepcopy(num_crossword[x][y])
+                        for neighbour_word in num_crossword[n_x][n_y]:
+                            if neighbour_word not in all_words:
+                                all_words.append(neighbour_word)
+                        if len(all_words) == 2:
+                            if placed_words[all_words[0]][3] == placed_words[all_words[1]][3]:
+                                return False
+                        elif len(all_words) == 3:
+                            all_words = list(set(all_words) - (set(num_crossword[x][y]) & set(num_crossword[n_x][n_y])))
+                            first = all_words[0]
+                            second = all_words[1]
+                            if first != current_word_num:
+                                first, second = second, first
+                            if not ((is_start(first, x, y) and is_finish(second, n_x, n_y) or is_start(second, n_x, n_y)
+                                     and is_finish(first, x, y))):
+                                return False
+                    else:
+                        return False
+        return True
+
+    place_word(inp[real_intersections[0][0]], 50, 50, True)
+
+    while real_intersections:
+        found_connection = False
+        for real_intersection in real_intersections:
+            if real_intersection[0] in used_words and real_intersection[1] in used_words:
+                return None
+            if real_intersection[0] in used_words and real_intersection[1] not in used_words:
+                found_connection = True
+                for placed_word in placed_words:
+                    if placed_word[0] == real_intersection[0]:
+                        row_axis = placed_word[1] + (real_intersection[2] if placed_word[3] else -real_intersection[3])
+                        column_axis = placed_word[2] + (
+                            -real_intersection[3] if placed_word[3] else real_intersection[2])
+                        placed_words.append([real_intersection[1], row_axis, column_axis, not placed_word[3]])
+                        used_words.append(real_intersection[1])
+                        if not place_word(inp[real_intersection[1]], row_axis, column_axis, not placed_word[3]):
+                            return None
+                        break
+                real_intersections.remove(real_intersection)
+                break
+
+            elif real_intersection[1] in used_words and real_intersection[0] not in used_words:
+                for placed_word in placed_words:
+                    if placed_word[0] == real_intersection[1]:
+                        found_connection = True
+                        row_axis = placed_word[1] + (real_intersection[3] if placed_word[3] else -real_intersection[2])
+                        column_axis = placed_word[2] + (
+                            -real_intersection[2] if placed_word[3] else real_intersection[3])
+                        placed_words.append([real_intersection[0], row_axis, column_axis, not placed_word[3]])
+                        used_words.append(real_intersection[0])
+                        if not place_word(inp[real_intersection[0]], row_axis, column_axis, not placed_word[3]):
+                            return None
+                        break
+                real_intersections.remove(real_intersection)
+                break
+        if not found_connection:
+            map_crossword = [['.' for _ in range(100)] for _ in range(100)]
+            num_crossword = [[[] for _ in range(100)] for _ in range(100)]
+            placed_words = [[real_intersections[0][0], 50, 50, True]]
+            used_words = [real_intersections[0][0]]
+    return map_crossword
+
+
+def check_crossword(intersections_par, real_intersections_par=None, printable=False):
     if not intersections_par:
-        # print(real_intersections_par)
+        solution = check_crossword_build(real_intersections_par)
+        if not solution:
+            return False
+        if printable:
+            left_index = 100
+            right_index = 0
+            up_index = 100
+            down_index = 0
+            for i in range(100):
+                for j in range(100):
+                    if solution[i][j] != '.':
+                        left_index = min(left_index, j)
+                        right_index = max(right_index, j)
+                        up_index = min(up_index, i)
+                        down_index = max(down_index, i)
+            if right_index - left_index > 19 or down_index - up_index > 19:
+                return False
+            res_crossword = [['.' for _ in range(20)] for _ in range(20)]
+            for i in range(up_index, down_index + 1):
+                for j in  range(left_index, right_index + 1):
+                    res_crossword[i - up_index][j - left_index] = solution[i][j]
+            for i in res_crossword:
+                for j in i:
+                    print(j, end=' ')
+                print()
         return True
     intersections = copy.deepcopy(intersections_par)
     real_intersections = copy.deepcopy(real_intersections_par) if real_intersections_par is not None else []
@@ -27,32 +157,31 @@ def check_crossword(intersections_par, real_intersections_par=None):
         used.append([real_intersection[1], real_intersection[3]])
 
     intersection = intersections[0]
-    has_solution = False
     for letter_a in range(len(inp[intersection[0]])):
         for letter_b in range(len(inp[intersection[1]])):
             if inp[intersection[0]][letter_a] == inp[intersection[1]][letter_b]:
-                # print(inp[intersection[0]], inp[intersection[1]])
                 if [intersection[0], letter_a] not in used and [intersection[1], letter_b] not in used:
-                    # add check crossword build
+                    # if not check_crossword_build(real_intersections):
+                    #     return False
                     real_intersections.append([intersection[0], intersection[1], letter_a, letter_b])
-                    has_solution += check_crossword(intersections[1:], real_intersections)
+                    if check_crossword(intersections[1:], real_intersections, printable):
+                        return True
                     real_intersections.pop()
-    # print(real_intersections)
-    return has_solution
+    return False
 
 
 def choose_best_crosswords(crosswords_par, num_of_best):
     fitness_function = []
 
     def dfs(node):
-        cnt = 0
+        cnt = 1
         used[node] = True
         for destination in crossword:
             if destination[0] == node and not used[destination[1]]:
                 cnt += dfs(destination[1])
             if destination[1] == node and not used[destination[0]]:
                 cnt += dfs(destination[0])
-        return cnt + 1
+        return cnt
 
     for crossword in crosswords_par:
         used = [False] * n
@@ -63,13 +192,10 @@ def choose_best_crosswords(crosswords_par, num_of_best):
                 fitness += num * num
         fitness_function.append([fitness, len(fitness_function)])
     fitness_function.sort(key=lambda x: x[0], reverse=True)
-    # check_crossword(crosswords[fitness_function[0][1]])
-    print(fitness_function[0][0])
-    # print(crosswords[fitness_function[0][1]])
     best_crosswords_found = []
     for best_i in range(num_of_best):
         best_crosswords_found.append(crosswords[fitness_function[best_i][1]])
-    return best_crosswords_found
+    return [best_crosswords_found, fitness_function[0][0]]
 
 
 def crosswords_crossover(crossword_a_par, crossword_b_par, longer):
@@ -101,24 +227,36 @@ def crosswords_mutation(initial_crosswords):
     res_crosswords = []
     for crossword in initial_crosswords:
         proper_crossword = False
+        possible_mutations = [elem for elem in possible_intersections if elem not in crossword]
         while not proper_crossword:
             random_choice = random.random()
-            if random_choice <= 0.25:
+            if random_choice <= 0.25 and crossword:
                 crossword.remove(random.choice(crossword))
                 break
             elif random_choice <= 0.5:
-                while True:
-                    crossword.append(random.choice(possible_intersections))
+                while possible_mutations:
+                    new_intersection = random.choice(possible_mutations)
+                    possible_mutations.remove(new_intersection)
+                    crossword.append(new_intersection)
                     if check_crossword(crossword):
                         proper_crossword = True
                         break
                     else:
                         crossword.pop()
-            elif random_choice <= 0.75:
+                        if random.random() <= 0.1:
+                            proper_crossword = True
+                            break
+            elif random_choice <= 0.75 and crossword:
                 random_ind = random.randint(0, len(crossword) - 1)
-                while True:
-                    crossword[random_ind] = random.choice(possible_intersections)
+                while possible_mutations:
+                    new_intersection = random.choice(possible_mutations)
+                    possible_mutations.remove(new_intersection)
+                    crossword[random_ind] = new_intersection
                     if check_crossword(crossword):
+                        proper_crossword = True
+                        break
+                    elif random.random() <= 0.1:
+                        crossword.pop(random_ind)
                         proper_crossword = True
                         break
             else:
@@ -133,7 +271,6 @@ for line in fileinput.input(files='input.txt'):
 n = len(inp)
 possible_intersections = find_possible_intersections(inp)
 crosswords = []
-# print(possible_intersections)
 for i in range(n * n):
     while True:
         random_crossword = []
@@ -149,10 +286,14 @@ for i in range(n * n):
             break
 
 while True:
-    best_crosswords = choose_best_crosswords(crosswords, n)
+    [best_crosswords, best_fitness_function] = choose_best_crosswords(crosswords, n)
+    print(best_fitness_function)
+    print(best_crosswords[0])
+    check_crossword(best_crosswords[0], None, True)
     crosswords = copy.deepcopy(best_crosswords)
     for i in range(n):
         for j in range(i + 1, n):
             crosswords.append(crosswords_crossover(best_crosswords[i], best_crosswords[j], True))
             crosswords.append(crosswords_crossover(best_crosswords[i], best_crosswords[j], False))
+
     crosswords = crosswords_mutation(crosswords)
--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-arr = [1]
-arr.pop()
-print(arr)
-if not arr:
-    print(143)
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 5, 8, 7]
+
+all_elements = list1[:]
+for elem in list2:
+    if elem not in all_elements:
+        all_elements.append(elem)
+print(all_elements)
+common_elements = list(set(list1) & set(list2))
+print(common_elements)
+print(list(set(all_elements) - set(common_elements)))
